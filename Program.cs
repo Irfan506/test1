b29b7be6e406f62a7767db50e8d7a009c5082af7
Irfan Uddin <59964467+Irfan506@users.noreply.github.com>
2020-12-14 10:48:29 +0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            towerofhanoi ob1 = new towerofhanoi();
+            towerofhanoi ob2 = new towerofhanoi();
+            towerofhanoi ob3 = new towerofhanoi();
+            for(int i=0;i<3;i++)
+            {
+                var t = (Console.ReadLine()).Split(' ');
+                ob1.add(Convert.ToChar(t[0]));
+                ob2.add(Convert.ToChar(t[1]));
+                ob3.add(Convert.ToChar(t[2]));
+            }
+            while(true)
+            {
+                if (ob1.check() == 1 && ob2.check() == 1)
+                    break;
+                else
+                {
+                    char po;
+                    var te = (Console.ReadLine()).Split(' ');
+                    int a = int.Parse(te[0]);
+                    int b = int.Parse(te[1]);
+                    if (a == 1)
+                    {
+                        if (ob1.stk.Count > 0)
+                            po = ob1.stk.Pop();
+                    }
+                    else
+                        if (a == 2)
+                    {
+                        if (ob2.stk.Count > 0)
+                            po = ob2.stk.Pop();
+                    }
+                        
+                    else
+                    {
+                        if (ob2.stk.Count > 0)
+                            po = ob3.stk.Pop();
+                    }
+                        
+                    if (b == 1)
+                        ob1.stk.Push(po);
+                    else
+                        if (b == 2)
+                        ob2.stk.Push(po);
+                    else
+                        ob2.stk.Push(po);
+
+                }
+                    
+            }
+        }
+    }
+}
---
+++ towerofhanoi.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace project
+{
+    public class towerofhanoi
+    {
+        public Stack<char> stk = new Stack<char>();
+        public void add(char ch)
+        {
+            stk.Push(ch);
+        }
+        public void print()
+        {
+            while (stk.Count > 0)
+                Console.Write(stk.Pop());
+        }
+        public int check()
+        {
+            Stack<char> stk1 = new Stack<char>();
+            stk1 = stk;
+            if (stk.Count != 3)
+                return 0;
+            char ch1 = stk1.Pop();
+            char ch2 = stk1.Pop();
+            char ch3 = stk1.Pop();
+            if (ch1.Equals(ch2) && ch1.Equals(ch3))
+            {
+                Console.WriteLine("Khela shesh");
+                return 1;
+            }
+                      
+            else
+                return 0;
+        }
+    }
+}
